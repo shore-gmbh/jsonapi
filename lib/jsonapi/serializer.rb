d656c6c4332d5711666b9adeec233e0a6a119fa1
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2017-06-25 20:13:02 +0200

    Add relationships to schema

--- lib/jsonapi/serializer.rb
+++ lib/jsonapi/serializer.rb
@@ -23,5 +23,32 @@ module Jsonapi
         attributes[attribute] = data[attribute]
       end
     end
+
+    def relationships
+      schema.relationships.each_with_object({}) do |(name, conf), relationships|
+        relationships[name] = send(
+          "build_#{conf[:cardinality]}_relationship",
+          conf[:type],
+          name,
+          data
+        )
+      end
+    end
+
+    private
+
+    def build_one_relationship(type, name, data)
+      { data: data_for(data["#{name}_id".to_sym], type)[0] }
+    end
+
+    def build_many_relationship(type, name, data)
+      { data: data_for(data["#{name}_ids".to_sym], type) }
+    end
+
+    def data_for(ids, type)
+      Array(ids).map do |id|
+        { type: type, id: id }
+      end
+    end
   end
 end
